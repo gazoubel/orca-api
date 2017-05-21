197bd02008a0de175cee0c8f1cc646fe496140b1
gazoubel <gazoubel@gmail.com>
2017-05-20 23:11:13 -0400

    added laborites

--- api/models/LaborItem.js
+++ api/models/LaborItem.js
@@ -1,13 +1,22 @@
 module.exports = {
   attributes: {
+    // key:{
+    //   type: "string",
+    //   required: true,
+    //   unique: true
+    // },
     name: {
       type: "string",
       required: true,
-      unique: true
+      // unique: true
     },
     company:{
       model: 'Company',
       required: true
     }
-  }
+  },
+  // beforeValidation : function(values,cb) {
+  //   values.key = values.name+values.company.id;
+  //   cb();
+  // }
 };
